2e36a21b2b65e03ab385382f69765454231baab9
Иван Хомичков <ivan.khomichkov@gmail.com>
2017-03-20 18:20:08 +0300

    Acreated base abstract class for future Sorting classes.

---
+++ SortingAlgorithms/SortingAlgorithms/Sorting.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    abstract class Sorting
+    {
+        int[] array;
+        int arrayLength;
+        Sorting(int length)
+        {
+            this.arrayLength = length;
+        }
+        enum FillType
+        {
+            Increment,
+            Decrement,
+            Random
+        }
+        public int Comparisions { get; protected set; }
+        public int Transactions { get; protected set; }
+
+        public abstract void Sort();
+        
+
+        void Fill(FillType fillType)
+        {
+            switch (fillType)
+            {
+                case FillType.Increment:
+                    FillIncrement();
+                    break;
+                case FillType.Decrement:
+                    FillDecrement();
+                    break;
+                case FillType.Random:
+                    FillRandom();
+                    break;
+            }
+        }
+        void FillDecrement()
+        {
+            int temp = arrayLength - 1;
+            for (int i = 0; i < arrayLength; i++, temp--)
+            {
+                array[i] = temp;
+            }
+        }
+
+        void FillIncrement()
+        {
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = i;
+            }
+        }
+        void FillRandom()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-100, 100);
+            }
+        }
+    }
+}
